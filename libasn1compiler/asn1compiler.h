91be71ffadf61d4404e88ebbdfeecdef06039d5f
Michel Mooij <michel.mooij7@gmail.com>
2017-03-14 13:54:28 +0100

    Corrected typo (missing comma).

--- libasn1compiler/asn1compiler.h
+++ libasn1compiler/asn1compiler.h
@@ -77,7 +77,7 @@ enum asn1c_flags {
 	 * -fline-refs
 	 * Include ASN.1 module's line numbers in comments.
 	 */
-	A1C_LINE_REFS       = 0x20000
+	A1C_LINE_REFS       = 0x20000,
 	/*
 	 * -fno-skeletons
 	 * Do not symlink or copy any skeletons
